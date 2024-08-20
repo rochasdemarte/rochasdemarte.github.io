2d1ee0a2b129d58906ef4726c0b36a1c53097305
Pedro Rocha Martin <pedrorcontato@gmail.com>
2024-08-20 11:26:25 -0300

    updated cv

--- lang.js
+++ lang.js
@@ -12,9 +12,9 @@ var lang = {
         yourCha: ' SEU DESAFIO',
         bio: 'BIO',
         bioText0: 'Como aproximador convidativo Pedro Rocha Martin acompanha as oscilações e frequências que conflituam o mundo contemporâneo, busca e investiga maneiras de nutrir o campo relacional entre as realidades, as construções de símbolos, e as diversas esferas culturais.',
-        bioText1: 'Artista multimídia brasileiro, Bacharel em Artes Visuais pela Universidade Federal do Rio Grande do Sul, elenca em seu trabalho uma leitura atrelada ao envolvimento e à entrega do receptor. Em múltiplas plataformas compartilha a função de produtor na maneira de interagir e se relacionar com sua obra. Pedro se mune da linguagem audiovisual como suporte diretriz do desenvolvimento de seu trabalho e ferramenta de estudo e autoconhecimento, aliado ao uso desmistificador das novas mídias digitais assim como da eletrônica analógica, procura se comunicar direta e indiretamente com a carga subjetiva do receptor.',
+        bioText1: 'Artista multimídia brasileiro, Bacharel em Artes Visuais pela Universidade Federal do Rio Grande do Sul, elenca em seu trabalho uma leitura atrelada ao envolvimento e à entrega do receptor. Em múltiplas plataformas compartilha a função de produtor na maneira de interagir e se relacionar com sua obra. Pedro se mune da linguagem audiovisual como suporte diretriz do desenvolvimento de seu trabalho e ferramenta de estudo e autoconhecimento, aliado ao uso desmistificador das novas mídias digitais assim como da eletrônica analógica, procura se comunicar direta e indiretamente com a carga subjetiva do receptor. Em paralelo a desdobramentos práticos desta pesquisa se originam alguns de seus futuros trabalhos onde investiga som, luz, vídeo, e suas relações.',
         bioText2: 'Tem o início de sua trajetória na experimentação audiovisual. Onde adquiriu a experiência que o acompanha na construção de um olhar artístico curioso para o mundo.',
-        bioText3: 'Em 2008 começou sua trajetória trabalhando como montador e finalizador de vídeo. De 2013 até 2015, participa como assistente de direção da equipe da RITO Entretenimento Inteligente, trabalhando com criações artísticas, com produções audiovisuais e direção de palco e atores. Ainda em 2015 tem início sua pesquisa em eletrônica voltada para sua produção artística, em paralelo a desdobramentos práticos desta pesquisa se originam alguns de seus futuros trabalhos onde investiga som, luz, vídeo, e suas relações. Seguindo nessa busca e transitando por entre formatos realiza em 2018 a performance "GlitchLab", e no começo de 2019 a instalação "INSTANTE: retro-olho", na AntiExpo - Descuradoria Urbana. Também em 2019, participa da residência artística premiada no 13º Prêmio Açorianos de Artes Plásticas "As coisas que são ditas antes. Ocupação. Residência. Exposição", na Casa Baka, com a instalação "os olhos que me viram antes que eu te visse".',
+        bioText3: '"Tudo vibra. Creio que somos parte do movimento contínuo das coisas, e nisso se constrói minha curiosidade, e dessa curiosidade nasce a minha motivação. Caixas-pretas e seus funcionamentos sempre foram sedutores pra mim, e considerando um convite o pensamento de Arlindo Machado, não submeter-se às determinações do aparato técnico, e sim subverter continuamente a função da máquina, concordo que a arte é sempre produzida e incorporada ao espelho dos meios de seu tempo, acredito que minha busca se sustenta na descomplicação das barreiras do conhecimento instrumental, e na aproximação do fazer investigativo dado o presente estado de saturada abundância material. Som, imagem e interação são as palavras-chave centrais da minha pesquisa, e delas tem se desdobrado a densidade do meu trabalho. O que somos nós se não o resultado das relações e influências do meio misturadas as nossas reações, escolhas e interferências?"',
 
     },
     'en': {
@@ -29,10 +29,10 @@ var lang = {
         yourBrand: ' YOUR BRAND',
         yourCha: ' YOUR CHALLENGE',
         bio: 'BIO',
-        bioText0: 'Como aproximador convidativo Pedro Rocha Martin acompanha as oscilações e frequências que conflituam o mundo contemporâneo, busca e investiga maneiras de nutrir o campo relacional entre as realidades, as construções de símbolos, e as diversas esferas culturais.',
-        bioText1: 'Artista multimídia brasileiro, Bacharel em Artes Visuais pela Universidade Federal do Rio Grande do Sul, elenca em seu trabalho uma leitura atrelada ao envolvimento e à entrega do receptor. Em múltiplas plataformas compartilha a função de produtor na maneira de interagir e se relacionar com sua obra. Pedro se mune da linguagem audiovisual como suporte diretriz do desenvolvimento de seu trabalho e ferramenta de estudo e autoconhecimento, aliado ao uso desmistificador das novas mídias digitais assim como da eletrônica analógica, procura se comunicar direta e indiretamente com a carga subjetiva do receptor.',
-        bioText2: 'Tem o início de sua trajetória na experimentação audiovisual. Onde adquiriu a experiência que o acompanha na construção de um olhar artístico curioso para o mundo.',
-        bioText3: 'Em 2008 começou sua trajetória trabalhando como montador e finalizador de vídeo. De 2013 até 2015, participa como assistente de direção da equipe da RITO Entretenimento Inteligente, trabalhando com criações artísticas, com produções audiovisuais e direção de palco e atores. Ainda em 2015 tem início sua pesquisa em eletrônica voltada para sua produção artística, em paralelo a desdobramentos práticos desta pesquisa se originam alguns de seus futuros trabalhos onde investiga som, luz, vídeo, e suas relações. Seguindo nessa busca e transitando por entre formatos realiza em 2018 a performance "GlitchLab", e no começo de 2019 a instalação "INSTANTE: retro-olho", na AntiExpo - Descuradoria Urbana. Também em 2019, participa da residência artística premiada no 13º Prêmio Açorianos de Artes Plásticas "As coisas que são ditas antes. Ocupação. Residência. Exposição", na Casa Baka, com a instalação "os olhos que me viram antes que eu te visse".',
+        bioText0: 'As an inviting approacher, Pedro Rocha Martin follows the oscillations and frequencies that conflict the contemporary world, seeks and investigates ways to nourish the relational field between realities, the construction of symbols, and the diverse cultural spheres.',
+        bioText1: 'Brazilian multimedia artist, Bachelor of Visual Arts from the Federal University of Rio Grande do Sul, outlines in his work a reading linked to the involvement and delivery of the receiver. On multiple platforms, he shares the role of producer in the way he interacts and relates to his work. Pedro uses audiovisual language as a guiding support for the development of his work and a tool for study and self-knowledge, combined with the demystifying use of new digital media as well as analog electronics, he seeks to communicate directly and indirectly with the subjective load of the receiver. In parallel with the practical developments of this research, some of his future works originate, where he investigates sound, light, video, and their relationships.',
+        bioText2: 'He began his career in audiovisual experimentation, where he acquired the experience that accompanies him in the construction of a curious artistic view of the world.',
+        bioText3: '"Everything vibrates. I believe that we are part of the continuous movement of things, and this is where my curiosity is built, and from this curiosity my motivation is born. Black boxes and their functioning have always been seductive to me, and considering Arlindo Machado\'s thought as an invitation, not to submit to the determinations of the technical apparatus, but rather to continually subvert the function of the machine, I agree that art is always produced and incorporated into the mirror of the means of its time. I believe that my search is sustained by the simplification of the barriers of instrumental knowledge, and by the approximation of investigative work given the current state of saturated material abundance. Sound, image and interaction are the central keywords of my research, and from them the density of my work has unfolded. What are we if not the result of the relationships and influences of the environment mixed with our reactions, choices and interferences?"',
 
         
     },
@@ -43,8 +43,8 @@ var lang = {
         sobre: '关于',
         projeto: '项目',
         contato: '联系',
-        tellme: 'ME CONTE SOBRE',
-        yourProjecto: ' SEU PROJETO',
+        tellme: '告诉我',
+        yourProjecto: '您的项目',
         yourIdeia: ' SUA IDEIA',
         yourBrand: ' SUA MARCA',
         yourCha: ' SEU DESAFIO',
@@ -55,15 +55,15 @@ var lang = {
         bioText3: 'Em 2008 começou sua trajetória trabalhando como montador e finalizador de vídeo. De 2013 até 2015, participa como assistente de direção da equipe da RITO Entretenimento Inteligente, trabalhando com criações artísticas, com produções audiovisuais e direção de palco e atores. Ainda em 2015 tem início sua pesquisa em eletrônica voltada para sua produção artística, em paralelo a desdobramentos práticos desta pesquisa se originam alguns de seus futuros trabalhos onde investiga som, luz, vídeo, e suas relações. Seguindo nessa busca e transitando por entre formatos realiza em 2018 a performance "GlitchLab", e no começo de 2019 a instalação "INSTANTE: retro-olho", na AntiExpo - Descuradoria Urbana. Também em 2019, participa da residência artística premiada no 13º Prêmio Açorianos de Artes Plásticas "As coisas que são ditas antes. Ocupação. Residência. Exposição", na Casa Baka, com a instalação "os olhos que me viram antes que eu te visse".',
 
     },
-    //正體中文 - Chinês (Tradicional)
+    //繁體 - 正體中文 - Chinês (Tradicional)
     'zh-Hant': {
         hero: '我開發涉及藝術和技術的解決方案',
         herobtn: '所有項目',
         sobre: '關於',
         projeto: '项目',
         contato: '聯絡',
-        tellme: 'ME CONTE SOBRE',
-        yourProjecto: ' SEU PROJETO',
+        tellme: '告訴我',
+        yourProjecto: '您的項目',
         yourIdeia: ' SUA IDEIA',
         yourBrand: ' SUA MARCA',
         yourCha: ' SEU DESAFIO',
@@ -99,11 +99,11 @@ var lang = {
         sobre: 'SOBRE',
         projeto: 'PROYECTOS',
         contato: 'CONTACTO',
-        tellme: 'ME CONTE SOBRE',
-        yourProjecto: ' SEU PROJETO',
-        yourIdeia: ' SUA IDEIA',
-        yourBrand: ' SUA MARCA',
-        yourCha: ' SEU DESAFIO',
+        tellme: 'CUÉNTAME SOBRE',
+        yourProjecto: ' TU PROYECTO',
+        yourIdeia: ' TU IDEA',
+        yourBrand: ' TU MARCA',
+        yourCha: ' TU DESAFÍO',
         bio: 'BIO',
         bioText0: 'Como aproximador convidativo Pedro Rocha Martin acompanha as oscilações e frequências que conflituam o mundo contemporâneo, busca e investiga maneiras de nutrir o campo relacional entre as realidades, as construções de símbolos, e as diversas esferas culturais.',
         bioText1: 'Artista multimídia brasileiro, Bacharel em Artes Visuais pela Universidade Federal do Rio Grande do Sul, elenca em seu trabalho uma leitura atrelada ao envolvimento e à entrega do receptor. Em múltiplas plataformas compartilha a função de produtor na maneira de interagir e se relacionar com sua obra. Pedro se mune da linguagem audiovisual como suporte diretriz do desenvolvimento de seu trabalho e ferramenta de estudo e autoconhecimento, aliado ao uso desmistificador das novas mídias digitais assim como da eletrônica analógica, procura se comunicar direta e indiretamente com a carga subjetiva do receptor.',
@@ -113,20 +113,33 @@ var lang = {
     }
 }
 
-// const userLang = localStorage.getItem('language') ? localStorage.getItem('language') : (navigator.language.substring(0,2).includes("pt") ? "pt" : "en");
-// const langBtn = document.getElementById('langBtn');
+const userLang = localStorage.getItem('language') ? localStorage.getItem('language') : (navigator.language.substring(0,2).includes("zn") ? navigator.language : navigator.language.substring(0,2));
+const ptLangBtn = document.getElementById('ptLangBtn');
+const langBtns = document.querySelectorAll(".lang-tbn");
 
-// function multilanguage(language) {
-//   localStorage.setItem('language', language);
-//   Object.keys(lang[language]).forEach((item, i) => {
-//     if (document.getElementById(item)) {
-//       document.getElementById(item).innerHTML = lang[language][item];
+// if (localStorage.getItem('language')) {
+//     userLang = localStorage.getItem('language');
+// } else {
+//     if (navigator.language.substring(0,2).includes('zn')) {
+//         userLang = navigator.language;
+//     } else {
+//         userLang = navigator.language.substring(0,2);
 //     }
-//   });
 // }
+
+function multilanguage(language) {
+  localStorage.setItem('language', language);
+  Object.keys(lang[language]).forEach( item => {
+    if (document.getElementById(item)) {
+      document.getElementById(item).innerHTML = lang[language][item];
+    }
+  });
+}
 // multilanguage(userLang);
 
-// langBtn.onclick = () => {
-//   let newLang = localStorage.getItem('language') == 'pt' ? 'en' : 'pt';
-//   multilanguage(newLang);
-// }
+langBtns.forEach(btn => {
+    btn.onclick = () => {
+    let newLang = btn.dataset.lang;
+    multilanguage(newLang);
+  }
+});
